81b1665ec38208535d1a8404e217e2c2db282824
kly001 <karenly1961@gmail.com>
2019-09-13 13:03:42 -0400

    Setup state for strikes, balls, hits, fouls

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [strikes, setStrikes] = useState(0)
+  const [balls, setBalls] = useState(0)
+  const [hits, setHits] = useState(0)
+  const [fouls, setFouls] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> Web-Application-Testing Project</h1>
     </div>
   );
 }
